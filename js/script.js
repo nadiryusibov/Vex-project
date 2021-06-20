12ba3170869d41a99fd8c73ecafdc5e957278686
Nadir1993 <nadiryusibov950@gmail.com>
2021-06-20 17:51:54 +0400

    my commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.btn-menu i').click(function(){
+    $(this).toggleClass('fas fa-times');
+    $('.nav-bar-menu').toggleClass('active')
+  })
